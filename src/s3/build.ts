0f7c2616bb48830851e4cbd8e981b0225b899c6c
Guillaume Fradin <guillaumefradin@gmail.com>
2023-11-07 19:00:36 +0000

    add type for clarity

--- src/s3/build.ts
+++ src/s3/build.ts
@@ -1,6 +1,6 @@
 import {buildForS3} from './buildCommon.js';
 
 const argv = process.argv;
-const typescript = argv.includes('--typescript');
+const typescript: boolean = argv.includes('--typescript');
 
 buildForS3({typescript});
