91c5e2fd34e78e5cc58a9e51a761cdef7b46691b
Guillaume Fradin <guillaumefradin@gmail.com>
2023-11-07 17:10:19 +0000

    do not rely on lib folder

--- src/cleanBuild.ts
+++ src/cleanBuild.ts
@@ -1,26 +1,2 @@
-import {rimraf} from 'rimraf';
-
-const pathsToRemove = ['dist/polygonjs/backup'];
-
-// Function to remove a path using rimraf
-function removePath(path: string): Promise<void> {
-	return new Promise(async (resolve, reject) => {
-		const result = await rimraf(path);
-		if (!result) {
-			console.error(`Error removing the path ${path}:`, result);
-			return reject(result);
-		}
-		console.log(`Successfully removed ${path}`);
-		resolve();
-	});
-}
-
-// Function to remove all paths in the array
-function removeAllPaths(paths: string[]) {
-	return Promise.all(paths.map(removePath));
-}
-
-// Execute the path removals
-removeAllPaths(pathsToRemove).catch((/*err*/) => {
-	process.exit(1); // Exit with a failure code if any path removal fails
-});
+import {cleanBuild} from './cleanBuildCommon';
+cleanBuild();
